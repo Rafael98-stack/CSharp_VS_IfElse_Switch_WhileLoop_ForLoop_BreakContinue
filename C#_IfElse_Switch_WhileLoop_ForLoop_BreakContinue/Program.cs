d061379d76544e6b56dab20c8971800341b3ffdb
Rafael98-stack <rafaelcasapaojr@gmail.com>
2024-09-09 10:35:55 +0200

    Arrays Learned

--- C#_IfElse_Switch_WhileLoop_ForLoop_BreakContinue/Program.cs
+++ C#_IfElse_Switch_WhileLoop_ForLoop_BreakContinue/Program.cs
@@ -161,11 +161,94 @@ for ( i = 0;i <= 10; i++)
     Console.WriteLine(i);
 }
 
-// FOREACH LOOP 
+// FOREACH LOOP / ARRAYS
+
+// Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+// To declare an array, define the variable type with square brackets
 
 string[] names = { "Marco", "Giulio", "Luca" };
+
+int[] numbersRandom = { 1, 78, 5, 10 };
 
 foreach (string a in names)
 {
     Console.WriteLine($"Hello {a}");
+}
+
+foreach (int n in numbersRandom)
+{
+    Console.WriteLine($"My numbers {n}");
+}
+
+Console.WriteLine(numbersRandom.Length); // Array Length, To find out how many elements an array has
+
+numbersRandom[2] = 20; // Change an Array Element, to change the value of a specific element, refer to the index number
+
+Console.WriteLine(numbersRandom[2]); // Access the Elements of an Array, access an array element by referring to the index number
+
+// Other Ways to Create an Array
+
+// Create an array of four elements, and add values later
+string[] cars = new string[4];
+
+// Create an array of four elements and add values right away 
+string[] shoes = new string[3] { "adidas", "nike", "puma" };
+
+// Create an array of four elements without specifying the size 
+string[] carsType = new string[] { "Berlina", "SUV"};
+
+// Create an array of four elements, omitting the new keyword, and without specifying the size
+string[] friends = { "Luca", "Mario", "Francesco", "Iacopo" };
+
+// Sort an Array
+
+// .Sort(x) sorts an array alphabetically or in an ascending order
+
+Array.Sort(friends);
+foreach (string friend in friends)
+{
+    Console.WriteLine(friend);
+}
+
+Array.Sort(numbersRandom);
+foreach (int n in numbersRandom)
+{
+    Console.WriteLine(n);
+}
+
+// Using Max() , Min() , Sum()
+
+Console.WriteLine(numbersRandom.Max()); // Returns the largest value
+Console.WriteLine(numbersRandom.Min()); // Returns the smallest value
+Console.WriteLine(numbersRandom.Sum()); // Returns the sum of elements 
+
+/* MULTIDIMENSIONAL ARRAYS */
+// To access an element of a two-dimensional array, you must specify two indexes: one for the array, and one for the element inside that array
+
+int[,] multiDimNum = { {1,2,3 }, {4,5,6 } };
+
+Console.WriteLine("I'm the number 4 of the second array - " + multiDimNum[1,0]);
+Console.WriteLine("I'm the number 3 of the first array - " + multiDimNum[0, 2]);
+
+// The value of an element can also be changed
+
+multiDimNum[0,2] = 10;
+Console.WriteLine("I've changed - " + multiDimNum[0, 2]);
+
+// Loop through the elements of a two-dimensional array with a foreach loop is also available
+
+foreach (int n in multiDimNum)
+{
+    Console.WriteLine($"{n}");
+}
+Console.WriteLine();
+
+// Loop through the elements of a two-dimensional array with a for loop is also available
+
+for (int n = 0; n < multiDimNum.GetLength(0); n++) // have to use GetLength() instead of Length to specify how many times the loop should run
+{
+    for (int n2 = 0; n2 < multiDimNum.GetLength(1); n2++)
+    {
+        Console.WriteLine(multiDimNum[n,n2]);
+    }
 }
